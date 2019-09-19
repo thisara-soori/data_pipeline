72622c366134a4b2fcf80ac30a6d761dc811998d
thisara-soori <thisarasooriyarachchi@gmail.com>
2019-09-19 11:39:22 +0530

    Update run-ecs-task.py

--- run-ecs-task.py
+++ run-ecs-task.py
@@ -9,19 +9,22 @@ SUBNET2 = os.environ['SUBNET2']
 
 def lambda_handler(event, context):
     client = boto3.client('ecs')
-    response = client.run_task(
-    cluster=CLUSTER, 
-    launchType = 'FARGATE',
-    taskDefinition=TASK_DEF, 
-    count = 1,
-    platformVersion='LATEST',
-    networkConfiguration={
-        'awsvpcConfiguration': {
-            'subnets': [SUBNET1,SUBNET2],
-            'assignPublicIp': 'DISABLED'
-        }
-    })
-    arn = response["tasks"][0]['taskArn']
-    waiter = client.get_waiter('tasks_stopped')
-    waiter.wait(cluster='thisara-test-cluster', tasks=[arn],WaiterConfig={'Delay': 60})
-    exit(0)
+    try:
+        response = client.run_task(
+        cluster=CLUSTER, 
+        launchType = 'FARGATE',
+        taskDefinition=TASK_DEF, 
+        count = 1,
+        platformVersion='LATEST',
+        networkConfiguration={
+            'awsvpcConfiguration': {
+                'subnets': [SUBNET1,SUBNET2],
+                'assignPublicIp': 'DISABLED'
+            }
+        })
+        arn = response["tasks"][0]['taskArn']
+        waiter = client.get_waiter('tasks_stopped')
+        waiter.wait(cluster='thisara-test-cluster', tasks=[arn],WaiterConfig={'Delay': 60})
+        exit(0)
+    except:
+         exit(1)
