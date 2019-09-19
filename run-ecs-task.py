34530367aed7cf094cd812cb6c242b493ea2b9c8
thisara-soori <thisarasooriyarachchi@gmail.com>
2019-09-19 09:30:12 +0530

    Create run-ecs-task.py

---
+++ run-ecs-task.py
@@ -0,0 +1,27 @@
+import json
+import boto3
+import os
+
+CLUSTER = os.environ['CLUSTER']
+TASK_DEF = os.environ['TASK_DEF']
+SUBNET1 = os.environ['SUBNET1']
+SUBNET2 = os.environ['SUBNET2']
+
+def lambda_handler(event, context):
+    client = boto3.client('ecs')
+    response = client.run_task(
+    cluster=CLUSTER, 
+    launchType = 'FARGATE',
+    taskDefinition=TASK_DEF, 
+    count = 1,
+    platformVersion='LATEST',
+    networkConfiguration={
+        'awsvpcConfiguration': {
+            'subnets': [SUBNET1,SUBNET2],
+            'assignPublicIp': 'DISABLED'
+        }
+    })
+    arn = response["tasks"][0]['taskArn']
+    waiter = client.get_waiter('tasks_stopped')
+    waiter.wait(cluster='thisara-test-cluster', tasks=[arn],WaiterConfig={'Delay': 60})
+    exit(0)
