bf98280d305e882607143b3cfa41ed203010dc6a
thisara-soori <thisarasooriyarachchi@gmail.com>
2019-09-27 10:55:09 +0530

    Update check.py

--- check.py
+++ check.py
@@ -10,7 +10,7 @@ def sanity_check(date, bucket):
     s3 = s3fs.S3FileSystem()
 
     output = ge.from_pandas(
-        pq.ParquetDataset(f's3://{bucket}/usp/output/date={date}', filesystem=s3).read_pandas().to_pandas()
+        pq.ParquetDataset("s3://"+bucket+"/usp/output/date="+date, filesystem=s3).read_pandas().to_pandas()
     )
 
     output.expect_column_values_to_be_in_set('accommodation_ns', [100])
